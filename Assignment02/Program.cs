e02f18a695419a7a7936a9b4f72ded0f8b8f0490
anuniquee <98219151+anuniquee@users.noreply.github.com>
2022-02-11 11:25:17 +0530

    Add files via upload

---
+++ Assignment02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo01.Run.RunThis();
+        }
+    }
+}
